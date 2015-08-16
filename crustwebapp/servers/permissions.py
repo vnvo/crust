0e11d282fadf1c00bf01d92b2281b47151d57e12
Vahid <negahdari@parspooyesh.com>
2015-08-17 02:05:31 +0430

    added required permissions for server tree

---
+++ crustwebapp/servers/permissions.py
@@ -0,0 +1,29 @@
+from rest_framework import permissions
+
+class IsAdminOrGroupOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, group_obj):
+        if self.request.user.is_admin:
+            return True
+
+        if group_obj.supervisor == self.request.user:
+            return True
+
+        return False
+
+class IsAdminOrServerOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, server_obj):
+        if self.request.user.is_admin:
+            return True
+        if server_obj.server_group.supervisor == self.request.user:
+            return True
+
+        return False
+
+class IsAdminOrServerAccountOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, serveraccount_obj):
+        if self.request.user.is_admin:
+            return True
+        if serveraccount_obj.server.server_group.supervisor == self.request.user:
+            return True
+
+        return False

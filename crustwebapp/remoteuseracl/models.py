8b1c08b81633becdf2c19bd663fb95671d47f3d6
Vahid <negahdari@parspooyesh.com>
2015-12-12 13:43:15 +0330

    new halper for server tree

--- crustwebapp/remoteuseracl/models.py
+++ crustwebapp/remoteuseracl/models.py
@@ -46,3 +46,106 @@ class RemoteUserACL(models.Model):
                 del allow_dict[acl_target]
 
         return allow_dict
+
+    @classmethod
+    def get_filtered_server_groups(cls, remote_user):
+        acl_list = cls.objects.filter(remote_user=remote_user).all()
+        allow_list = set()
+        deny_list = set()
+
+        def add_server_group_from_server(acl, sg):
+            """
+            Only add to allow list if server is allowed, other case is handled
+            automatically when allowing/denying a server group directly
+            """
+            acl_target = sg
+            if acl.acl_action == 'allow':
+                allow_list.add(acl_target)
+
+        def add_server_group(acl, sg):
+            acl_target = sg
+            if acl.acl_action == 'allow':
+                allow_list.add(acl_target)
+            else:
+                deny_list.add(acl_target)
+
+        for acl in acl_list:
+            if acl.server_account:
+                sg = acl.server_account.server.server_group
+                add_server_group_from_server(acl, sg)
+
+            elif acl.server:
+                sg = acl.server.server_group
+                add_server_group_from_server(acl, sg)
+
+            else:
+                add_server_group(acl, acl.server_group)
+
+        return list(allow_list - deny_list)
+
+    @classmethod
+    def get_filtered_servers_by_group(cls, remote_user, server_group):
+        acl_list = cls.objects.filter(remote_user=remote_user).all()
+        allow_list = set()
+        deny_list = set()
+
+        def add_server_from_serveraccount(acl, server):
+            acl_target = server
+            if acl.acl_action == 'allow':
+                allow_list.add(acl_target)
+
+        for acl in acl_list:
+            if acl.server_account:
+                if acl.server_account.server.server_group != server_group:
+                    continue
+                add_server_from_serveraccount(acl, acl.server_account.server)
+
+            elif acl.server:
+                if acl.server.server_group != server_group:
+                    continue
+
+                if acl.acl_action == 'allow':
+                    allow_list.add(acl.server)
+                else:
+                    deny_list.add(acl.server)
+
+            else:
+                if acl.server_group != server_group:
+                    continue
+
+                if acl.acl_action == 'allow':
+                    for server in acl.server_group.server_set.all():
+                        allow_list.add(server)
+
+        return list(allow_list-deny_list)
+
+    @classmethod
+    def get_filtered_server_accounts_by_server(cls, remote_user, server):
+        acl_list = cls.objects.filter(remote_user=remote_user).all()
+        allow_list = set()
+        deny_list = set()
+
+        for acl in acl_list:
+            if acl.server_account:
+                if acl.server_account.server != server:
+                    continue
+                if acl.acl_action == 'allow':
+                    allow_list.add(acl.server_account)
+                else:
+                    deny_list.add(acl.server_account)
+
+            elif acl.server:
+                if acl.server != server:
+                    continue
+
+                for server_account in acl.server.serveraccount_set.all():
+                    if acl.acl_action == 'allow':
+                        allow_list.add(server_account)
+
+            else:
+                sg_servers = acl.server_group.server_set.filter(server=server)
+                if sg_servers and acl.acl_action == 'allow':
+                    for server_account in sg_servers[0].serveraccount_set.all():
+                        allow_list.add(server_account)
+
+        return list(allow_list-deny_list)

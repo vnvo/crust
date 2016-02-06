3a2c3a71c820589553e3cde4b6af222daf23c6de
Vahid <negahdari@parspooyesh.com>
2016-02-07 02:07:29 +0330

    fixed error on importing servers from csv with whitespace characters in content

--- crustwebapp/static/javascripts/servers/servers/controllers/import_servers.controller.js
+++ crustwebapp/static/javascripts/servers/servers/controllers/import_servers.controller.js
@@ -21,39 +21,40 @@
         vm.done_entries = 0;
 
         function importEntry(entry){
-            if(!entry.length){
+            if(!entry.length || entry[0]==''){
                 vm.done_entries += 1;
                 vm.import_messages.push('Finished');
                 $rootScope.$broadcast('server.created');
                 return;
             }
-            //console.log(entry);
             var server_info = vm.csv_header.reduce(
                 function(prev, info_key, i){
                     var val = entry[i];
+                    if(val==undefined)
+                        val='';       
+                    val = val.replace(/(?:\\[rn]|[\r\n]+)+/g, "");
                     if(info_key=='sshv2_port')
                         val = (val.length ? parseInt(val):22);
                     if(info_key=='telnet_port')
                         val = (val.length ? parseInt(val):23);
+
+                    info_key = info_key.replace(/(?:\\[rn]|[\r\n]+)+/g, "");
                     prev[info_key]=val;
                     return prev;
                 }, {}
             );
-            //console.log('adding server ...');
-            //console.log(server_info);
             Servers.create(server_info).then(
                 function(data, status, headers, config){
                     vm.done_entries += 1;
-                    if(vm.entries.length)
-                        importEntry(vm.entries.shift());
+                    if(vm.entries.length && vm.entries!= [""])
+                        importEntry(vm.entries.shift().split(','));
                 },
                 function(data, status, headers, config){
-                    //console.log(data);
                     vm.import_messages.push(
                         'Error on '+server_info['server_name']
                     );
                     if(vm.entries.length)
-                        importEntry(vm.entries.shift());
+                        importEntry(vm.entries.shift().split(','));
                     else{
                         vm.import_messages.push('Finished.');
                         $rootScope.$broadcast('server.created');

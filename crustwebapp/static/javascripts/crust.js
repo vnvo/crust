1f81b63eac327fc4877a1456d4886a87957a4138
Vahid <negahdari@parspooyesh.com>
2016-02-13 15:33:33 +0330

    added password strength meter for remote user

--- crustwebapp/static/javascripts/crust.js
+++ crustwebapp/static/javascripts/crust.js
@@ -101,6 +101,7 @@ function space2nbsp(){
     };
 };
 
+
 /// register directives
 angular
     .module('crust')
@@ -126,3 +127,34 @@ angular
             }
         };
     });
+
+angular
+    .module('crust')
+    .directive('passwordStrength', [
+        function() {
+            return {
+                require: 'ngModel',
+                restrict: 'E',
+                scope: {
+                    password: '=ngModel'
+                },
+                link: function(scope, elem, attrs, ctrl) {
+                    scope.$watch('password', function(newVal) {
+                        scope.strength = isSatisfied(newVal && newVal.length >= 8) +
+                            isSatisfied(newVal && /[A-z]/.test(newVal)) +
+                            isSatisfied(newVal && /(?=.*\W)/.test(newVal)) +
+                            isSatisfied(newVal && /\d/.test(newVal));
+                        function isSatisfied(criteria) {
+                            return criteria ? 1 : 0;
+                        }
+                    }, true);
+                },
+                template: '<div class="progress" style="margin-bottom:0px;">' +
+                    '<div class="progress-bar progress-bar-danger" style="width: {{strength >= 1 ? 25 : 0}}%"></div>' +
+                    '<div class="progress-bar progress-bar-warning" style="width: {{strength >= 2 ? 25 : 0}}%"></div>' +
+                    '<div class="progress-bar progress-bar-warning" style="width: {{strength >= 3 ? 25 : 0}}%"></div>' +
+                    '<div class="progress-bar progress-bar-success" style="width: {{strength >= 4 ? 25 : 0}}%"></div>' +
+                    '</div>'
+            };
+        }
+    ]);
